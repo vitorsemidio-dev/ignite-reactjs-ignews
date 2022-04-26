046b05577bc5621c1917fda9d255947f11d8f80a
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-04-25 21:02:36 -0300

    feat: shows data from Prismic in html

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,23 @@ import Prismic from "@prismicio/client";
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
+import { RichText } from "prismic-dom";
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Array<Post>;
+}
+
+export default function Posts({ posts }: PostsProps) {
+  // const posts = [];
+  console.log(posts);
   return (
     <>
       <Head>
@@ -12,49 +26,31 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>10 de abril de 2022</time>
-            <strong>
-              Prisma: uma das melhores coisas que já aconteceu no ecossistema?
-            </strong>
-            <p>
-              Um breve arquivo sobre a origem da ferramenta que facilitou o
-              acesso de databases e ampliou produtividade para o ecossistema
-              JavaScript/TypeScript
-            </p>
-          </a>
-          <a href="#">
-            <time>10 de abril de 2022</time>
-            <strong>
-              Prisma: uma das melhores coisas que já aconteceu no ecossistema?
-            </strong>
-            <p>
-              Um breve arquivo sobre a origem da ferramenta que facilitou o
-              acesso de databases e ampliou produtividade para o ecossistema
-              JavaScript/TypeScript
-            </p>
-          </a>
-          <a href="#">
-            <time>10 de abril de 2022</time>
-            <strong>
-              Prisma: uma das melhores coisas que já aconteceu no ecossistema?
-            </strong>
-            <p>
-              Um breve arquivo sobre a origem da ferramenta que facilitou o
-              acesso de databases e ampliou produtividade para o ecossistema
-              JavaScript/TypeScript
-            </p>
-          </a>
+          {posts?.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
 
+type PrismicTypePostResponse = {
+  title: any;
+  content: Array<{
+    type: "paragraph";
+    text: string;
+  }>;
+};
+
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const response = await prismic.query(
+  const response = await prismic.query<PrismicTypePostResponse>(
     [Prismic.Predicates.at("document.type", "post")],
     {
       fetch: ["post.title", "post.content"],
@@ -64,7 +60,29 @@ export const getStaticProps: GetStaticProps = async () => {
 
   console.log(JSON.stringify(response, null, 2));
 
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        },
+      ),
+    };
+  });
+
+  console.log(posts);
+
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
